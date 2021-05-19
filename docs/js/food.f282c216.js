d67297bd32ca809661764cf58da17906c62b7344
Ryan Y <ryany@paradigm-corp.com>
2021-05-19 17:28:37 -0400

    Release docs
    Release docs so we can start using this as a tool.
    
    Signed-off-by: Ryan Y <ryany@paradigm-corp.com>

---
+++ docs/js/food.f282c216.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["food"],{ae7f:function(o,e,n){"use strict";n.r(e);var t=n("7a23"),c={class:"food"},s=Object(t["f"])("h1",null,"This is the food page",-1);function d(o,e){return Object(t["o"])(),Object(t["d"])("div",c,[s])}const a={};a.render=d;e["default"]=a}}]);
+//# sourceMappingURL=food.f282c216.js.map
